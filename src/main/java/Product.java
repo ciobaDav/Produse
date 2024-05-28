44bd889ea2d5f36fde63e300140b0183c7025889
Ciobanu David <davidciobanu06082006@gmail.com>
2024-05-28 22:36:06 +0300

    first commit

---
+++ src/main/java/Product.java
@@ -0,0 +1,64 @@
+import java.time.LocalDate;
+
+public class Product {
+    private String name;
+    private int quantity;
+    private double price;
+    private boolean available;
+    private LocalDate expiryDate;
+
+    public Product(String name, int quantity, double price, boolean available, LocalDate expiryDate) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.available = available;
+        this.expiryDate = expiryDate;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", available=" + available +
+                ", expiryDate=" + expiryDate +
+                '}';
+    }
+}
---
+++ web/employees.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sscerbatiuc
+  Date: 5/16/2024
+  Time: 8:39 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
